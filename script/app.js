33937add31ecbed8e55b1e5514f303376046f322
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-12-09 08:24:28 -0300

    Remmovido Dropdown js adicionado dropdown CSS

--- script/app.js
+++ script/app.js
@@ -1,23 +1,22 @@
 const linksDropdown = document.querySelectorAll('.dropdown');
 
-const dropdownShow = (event) => {
 
-
-  const linkDrop = event.target.parentElement.children[1]
-  const imgLinkDrop = event.target.children[0]
+//    Dropdown Click
+// const dropdownShow = (event) => {
+//   const linkDrop = event.target.parentElement.children[1]
+//   const imgLinkDrop = event.target.children[0]
    
-  linkDrop.classList.toggle('visible')
-  
-  const isVisible = linkDrop.classList.contains('visible');
-  if (isVisible) {
-    imgLinkDrop.style.transform = 'rotate(180deg)'
-  } else {
-    imgLinkDrop.style.transform = 'rotate(0deg)'
-  }
+//   linkDrop.classList.toggle('visible')
 
-}
+//   const isVisible = linkDrop.classList.contains('visible');
+//   if (isVisible) {
+//     imgLinkDrop.style.transform = 'rotate(180deg)'
+//   } else {
+//     imgLinkDrop.style.transform = 'rotate(0deg)'
+//   }
+// }
 
 
-linksDropdown.forEach((link, index) => {
-  link.addEventListener('click', dropdownShow)
-})
+// linksDropdown.forEach((link, index) => {
+//   link.addEventListener('click', dropdownShow)
+// })

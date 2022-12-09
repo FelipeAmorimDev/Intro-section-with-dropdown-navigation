c6ade560e537ba62d64bdd5fa0017118fc5ad5f7
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-12-09 08:13:33 -0300

    Adicionado efeito nos dropdown

--- script/app.js
+++ script/app.js
@@ -1,10 +1,15 @@
 const linksDropdown = document.querySelectorAll('.dropdown');
 
 const dropdownShow = (event) => {
+
+
   const linkDrop = event.target.parentElement.children[1]
   const imgLinkDrop = event.target.children[0]
+   
   linkDrop.classList.toggle('visible')
-  if (linkDrop.classList.contains('visible')) {
+  
+  const isVisible = linkDrop.classList.contains('visible');
+  if (isVisible) {
     imgLinkDrop.style.transform = 'rotate(180deg)'
   } else {
     imgLinkDrop.style.transform = 'rotate(0deg)'

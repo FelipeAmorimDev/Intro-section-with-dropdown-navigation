fff1f91a9b1d6837185c8c513017bb0c463852e9
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-12-10 05:00:13 -0300

    app.js refatorado, css refatorado

--- script/app.js
+++ script/app.js
@@ -6,44 +6,51 @@ const seta = document.querySelectorAll('.dropdown > a > img')
 function menuMobile() {
   menu.classList.toggle('menucliked')
   const isCliked = menu.classList.contains('menucliked')
-
   if (isCliked) {
     menuHb.setAttribute('src', './images/icon-close-menu.svg')
-    linksDropdown.forEach((item) => {
-      item.addEventListener('click', dropdownShow)
-    })
-  } else {
+    } else {
     menuHb.setAttribute('src', './images/icon-menu.svg')
   }
 }
 
 //    Dropdown Click
 const dropdownShow = (event) => {
-  event.stopPropagation()
+ 
   const linkDrop = event.target.parentElement.children[1]
   const imgLinkDrop = event.target.children[0]
+  const isDropElement = event.target.classList.contains('drop')
 
-  if (menu.classList.contains('menucliked')) {
-    linkDrop.classList.toggle('visible')
-  }
+  if (isDropElement) {
+    if (menu.classList.contains('menucliked')) {
+      linkDrop.classList.toggle('visible')
+    }
+    const isVisible = linkDrop.classList.contains('visible');
 
-  const isVisible = linkDrop.classList.contains('visible');
-  if (isVisible) {
-    imgLinkDrop.style.transform = 'rotate(180deg)'
-  } else {
-    imgLinkDrop.style.transform = 'rotate(0deg)'
+    if (isVisible) {
+      imgLinkDrop.style.transform = 'rotate(180deg)'
+    } else {
+      imgLinkDrop.style.transform = 'rotate(0deg)'
+    }
   }
 }
 
-document.addEventListener('click', (event) => {
-  if (!(event.target.parentElement.classList[0] === 'header__nav-item')) {
-    linksDropdown.forEach(element => {
-      element.children[1].classList.remove('visible')
+window.onclick = (event) => {
+  const dropsVisible = document.querySelectorAll('.visible')
+  if (!(event.target.matches('.drop'))) {
+    dropsVisible.forEach(element => {
+      const isVisible = element.classList.contains('visible');
+      if (isVisible) {
+        const arrowElement = element.parentElement.children[0].children[0]
+        element.classList.remove('visible')
+        arrowElement.style.transform = 'rotate(0deg)'
+      }
     })
-    seta.forEach(element => {
-      element.style.transform = 'rotate(0deg)'
-    })
+
   }
-})
+}
 
 menuHb.addEventListener('click', menuMobile)
+
+linksDropdown.forEach((item) => {
+  item.addEventListener('click', dropdownShow)
+})

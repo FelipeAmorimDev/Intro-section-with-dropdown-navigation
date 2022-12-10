e1046824ec1e08ef910efc792f9c63ea7083e6bf
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-12-10 02:38:39 -0300

    Adicionado sistema que fechar o dropdown caso seja clicado em outra area

--- script/app.js
+++ script/app.js
@@ -1,36 +1,49 @@
 const linksDropdown = document.querySelectorAll('.dropdown');
 const menuHb = document.querySelector('.menuhb');
 const menu = document.querySelector('.header__nav')
+const seta = document.querySelectorAll('.dropdown > a > img')
 
-function menuMobile () {
+function menuMobile() {
   menu.classList.toggle('menucliked')
   const isCliked = menu.classList.contains('menucliked')
-  
-  if(isCliked){
-    menuHb.setAttribute('src','./images/icon-close-menu.svg')
-  }else{
-    menuHb.setAttribute('src','./images/icon-menu.svg')
+
+  if (isCliked) {
+    menuHb.setAttribute('src', './images/icon-close-menu.svg')
+    linksDropdown.forEach((item) => {
+      item.addEventListener('click', dropdownShow)
+    })
+  } else {
+    menuHb.setAttribute('src', './images/icon-menu.svg')
   }
 }
 
-menuHb.addEventListener('click', menuMobile)
-
 //    Dropdown Click
-// const dropdownShow = (event) => {
-//   const linkDrop = event.target.parentElement.children[1]
-//   const imgLinkDrop = event.target.children[0]
-   
-//   linkDrop.classList.toggle('visible')
+const dropdownShow = (event) => {
+  event.stopPropagation()
+  const linkDrop = event.target.parentElement.children[1]
+  const imgLinkDrop = event.target.children[0]
 
-//   const isVisible = linkDrop.classList.contains('visible');
-//   if (isVisible) {
-//     imgLinkDrop.style.transform = 'rotate(180deg)'
-//   } else {
-//     imgLinkDrop.style.transform = 'rotate(0deg)'
-//   }
-// }
+  if (menu.classList.contains('menucliked')) {
+    linkDrop.classList.toggle('visible')
+  }
 
+  const isVisible = linkDrop.classList.contains('visible');
+  if (isVisible) {
+    imgLinkDrop.style.transform = 'rotate(180deg)'
+  } else {
+    imgLinkDrop.style.transform = 'rotate(0deg)'
+  }
+}
 
-// linksDropdown.forEach((link, index) => {
-//   link.addEventListener('click', dropdownShow)
-// })
+document.addEventListener('click', (event) => {
+  if (!(event.target.parentElement.classList[0] === 'header__nav-item')) {
+    linksDropdown.forEach(element => {
+      element.children[1].classList.remove('visible')
+    })
+    seta.forEach(element => {
+      element.style.transform = 'rotate(0deg)'
+    })
+  }
+})
+
+menuHb.addEventListener('click', menuMobile)

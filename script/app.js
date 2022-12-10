5de07661d5da82a8ff9b64adf47a8d7acf0948c3
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-12-10 20:37:35 -0300

    Adicionado sistema que fechar o menu mobile, quando acontece o clique em um link do nav

--- script/app.js
+++ script/app.js
@@ -1,6 +1,7 @@
 const linksDropdown = document.querySelectorAll('.dropdown');
 const menuBtn = document.querySelector('.menuhb');
 const navMenu = document.querySelector('.header__nav')
+const linksMenu = document.querySelectorAll('.header__wrapper a:not(.drop)')
 
 function menuMobile() {
   const isCliked = navMenu.classList.toggle('menucliked')
@@ -12,6 +13,13 @@ function menuMobile() {
     menuBtn.style.transform = 'rotate(-180deg)'
     menuBtn.setAttribute('src', './images/icon-menu.svg')
     document.body.style.overflowY = 'visible'
+  }
+}
+
+function closeMenu(){
+  const isMenuOpened  = navMenu.classList.contains("menucliked")
+  if(isMenuOpened){
+    navMenu.classList.remove('menucliked')
   }
 }
 
@@ -34,20 +42,21 @@ const dropdownShow = ({ target }) => {
 window.onclick = (event) => {
   const dropdownsOpened = document.querySelectorAll('.visible')
   const isDropdown = event.target.matches('.drop');
-  const isDropLink = event.target.matches('a');
+  const isDropLink = event.target.matches('a'); // adicionado para impedir de minimizar o dropdown caso fosse clicado em alguns dos seus link internos
   if (!isDropdown) {
-    if (!isDropLink) {
-      dropdownsOpened.forEach(dropdown => {
-        const arrowElement = dropdown.parentElement.children[0].children[0]
-        dropdown.classList.remove('visible')
-        arrowElement.classList.toggle("arrowcliked")
-      })
-    }
-
+    dropdownsOpened.forEach(dropdown => {
+      const arrowElement = dropdown.parentElement.children[0].children[0]
+      dropdown.classList.remove('visible')
+      arrowElement.classList.toggle("arrowcliked")
+    })
   }
 }
 
 menuBtn.addEventListener('click', menuMobile)
 linksDropdown.forEach((item) => {
   item.addEventListener('click', dropdownShow)
+})
+
+linksMenu.forEach(link => {
+  link.addEventListener('click',closeMenu)
 })
